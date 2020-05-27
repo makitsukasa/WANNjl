6c3c26111867164f82523e03f5def40ef4625d15
makitsukasa <allium.fistulosum.39@gmail.com>
2020-05-28 02:11:26 +0900

    resize 16x16

--- main.jl
+++ main.jl
@@ -2,13 +2,17 @@ include("./wann.jl")
 using LinearAlgebra: transpose!
 using Flux: onehot
 using Flux.Data.MNIST
+using Images: imresize
 
-n_sample = 100
-n_pop = 100
+n_sample = 1000
+n_pop = 960
+image_size = 16
 
 # convert Array{Array{ColorTypes.Gray{FixedPointNumbers.Normed{UInt8,8}},2},1} into Array{Float64,2}
-imgs = zeros(Float64, (n_sample, 28^2))
-transpose!(imgs, hcat(vec.(float.(MNIST.images()))[1:n_sample, :]...))
+imgs = zeros(Float64, (n_sample, image_size^2))
+# imgs_f = convert(Vector{Matrix{Float64}}, MNIST.images())
+imgs_f = convert(Vector{Matrix{Float64}}, map(i -> imresize(i, (image_size, image_size)), MNIST.images()))
+transpose!(imgs, hcat(vec.(imgs_f)[1:n_sample, :]...))
 # convert Array{Int64,1} into Array{Flux.OneHotVector,2}
 hoge = map(x -> onehot(x, 0:9), MNIST.labels()[1:n_sample, :])
 labels = hcat([[hoge[y][x] ? 1.0 : 0.0 for y = 1:n_sample] for x = 1:10]...)
@@ -29,7 +33,7 @@ hyp = Dict(
 	"prob_crossover" => 0.0
 )
 
-pop = WANN.Pop(28^2, 10, n_pop, hyp["prob_initEnable"])
+pop = WANN.Pop(image_size^2, 10, n_pop, hyp["prob_initEnable"])
 println("train")
 WANN.train(pop, imgs, labels, 100, hyp)
 
